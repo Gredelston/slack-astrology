7952a21e04d8128b680b9288a5eb7cd66af8ca61
Greg Edelston <gredelston@gmail.com>
2017-12-11 15:04:42 -0500

    test logging of request body

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require('express'),
+      bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
 app.post('/', function (req, res) {
-    res.send('Hello POST!');
+    console.log(req.body);
+    res.send(req.body.text);
 });
 
 app.listen(PORT, () => console.log('Example app listening on port ' + PORT + '!'));
