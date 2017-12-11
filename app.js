4814b42c3f33d4af6139fbc295010f17253318b1
Greg Edelston <gredelston@gmail.com>
2017-12-11 15:31:01 -0500

    Fix bodyParser to use urlencoded

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const express = require('express'),
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
 app.post('/', function (req, res) {
-    console.log(req);
-    res.send('Hello, POST');
+    console.log(req.body);
+    res.send('Hello, ' + req.body.text + '!');
 });
 
 app.listen(PORT, () => console.log('Example app listening on port ' + PORT + '!'));
